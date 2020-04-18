1080585936ec30bde658de09479f9c1f646d32b3
talios0 <20057606+talios0@users.noreply.github.com>
2020-04-18 00:30:41 -0400

    Cube Rotation and Camera Zoom

---
+++ Game/Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float maxZoom;
+    public float minZoom;
+
+    public float sensitivity;
+    public float interpTime;
+    public float snapAmount;
+
+    private Vector3 zoomTo;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            zoomTo = new Vector3(transform.position.x, transform.position.y, transform.position.z + Input.GetAxis("Mouse ScrollWheel") * -sensitivity);
+            zoomTo.z = Mathf.Clamp(zoomTo.z, minZoom, maxZoom);
+        }
+        if (zoomTo.z == 0) return;
+        transform.position = Vector3.Lerp(transform.position, zoomTo, interpTime);
+        if (Mathf.Abs(transform.position.z - zoomTo.z) < snapAmount) {
+            transform.position = zoomTo;
+            zoomTo.z = 0;
+        }
+    }
+}
--- Game/Assets/Scripts/Rotation/RotateCube.cs
+++ Game/Assets/Scripts/Rotation/RotateCube.cs
@@ -5,12 +5,14 @@ public class RotateCube : MonoBehaviour
     public bool active;
     public float interpTime;
     public float snapAmount;
+    public float sensitivity;
 
     private MouseState clickState;
     private Vector3 rotateTo;
+    private Vector3 previousRotation;
 
     private void Start() {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
         rotateTo = transform.eulerAngles;
     }
 
@@ -23,19 +25,28 @@ public class RotateCube : MonoBehaviour
     {
         if (!active) return;
         if (Input.GetMouseButtonDown(0) && (clickState == MouseState.NONE || clickState == MouseState.WAIT)) { clickState = MouseState.CLICK; }
-        else if (clickState == MouseState.CLICK && Input.GetMouseButtonDown(0)) clickState = MouseState.DRAG;
-        else if (clickState == MouseState.DRAG) {
-            if (!Input.GetMouseButtonDown(0)) { clickState = MouseState.NONE;}
+        else if (clickState == MouseState.CLICK && Input.GetMouseButton(0)) { clickState = MouseState.DRAG; }
+        else if (clickState == MouseState.DRAG)
+        {
+            if (!Input.GetMouseButton(0)) { clickState = MouseState.NONE; }
         }
         DragCube();
     }
 
     private void DragCube() {
-        if (clickState == MouseState.DRAG) rotateTo = new Vector3(transform.eulerAngles.x + Input.GetAxis("Mouse Y"), transform.eulerAngles.y + Input.GetAxis("Mouse X"), transform.eulerAngles.z);
-        if (Mathf.Abs(transform.eulerAngles.x - rotateTo.x) < snapAmount && Mathf.Abs(transform.eulerAngles.y - rotateTo.y) < snapAmount) {
-            clickState = MouseState.WAIT;
-            return;
+        if (clickState == MouseState.DRAG)
+        {
+            rotateTo = new Vector3(transform.eulerAngles.x + Input.GetAxis("Mouse Y") * -sensitivity, transform.eulerAngles.y + Input.GetAxis("Mouse X") * -sensitivity, transform.eulerAngles.z);
+            if (rotateTo.x < 175 && rotateTo.x > 85) rotateTo.x = 85;
+            else if (rotateTo.x > 185 && rotateTo.x < 275) rotateTo.x = 275;
         }
+        else if (clickState == MouseState.WAIT) return;
+        else if (clickState == MouseState.NONE && Mathf.Abs(transform.eulerAngles.x - rotateTo.x) < snapAmount && Mathf.Abs(transform.eulerAngles.y - rotateTo.y) < snapAmount) 
+        {
+            clickState = MouseState.WAIT;
+            transform.eulerAngles = rotateTo;
+        }
+        previousRotation = transform.eulerAngles;
         transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, rotateTo, interpTime);
     }
 }
